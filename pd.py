fd4d883ae4e8d5dd132d58ed8d12f08d5f78ee36
rudi48 <reuterr@web.de>
2016-10-19 10:23:31 +0200

    Update pd.py

--- pd.py
+++ pd.py
@@ -1,7 +1,7 @@
 ##
 ## This file is part of the libsigrokdecode project.
 ##
-## Copyright (C) 2013 Uwe Hermann <uwe@hermann-uwe.de>
+## Copyright (C) 2016 Rudolf Reuter <reuterru@arcor.de>
 ##
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -20,86 +20,51 @@
 
 import sigrokdecode as srd
 
-'''
-OUTPUT_PYTHON format:
-
-Packet:
-[<ptype>, <pdata>]
-
-<ptype>, <pdata>
- - 'ITEM', [<item>, <itembitsize>]
- - 'WORD', [<word>, <wordbitsize>, <worditemcount>]
-
-<item>:
- - A single item (a number). It can be of arbitrary size. The max. number
-   of bits in this item is specified in <itembitsize>.
-
-<itembitsize>:
- - The size of an item (in bits). For GPIB 8 data + 8 control.
-
-<sample_total>;
- - Total number of samples, needed to show the last byte
-
-<debug>:
- - Flag to enable debug prints
-
-2016-09-15, Rudolf Reuter, GPIB decoding of 16 bits,
-            derived from decoder "parallel".
-            Pins CLK, DAV, EOI, ATN must be set for proper decoding.
-
-'''
-
-class ChannelError(Exception):
-    pass
-
 class Decoder(srd.Decoder):
     api_version = 2
     id = 'gpib'
-    name = 'gpib'
+    name = 'GPIB'
     longname = 'General Purpose Interface Bus'
-    desc = 'GPIB, HPIB, IEEE488.'
+    desc = 'IEEE-488 GPIB / HPIB protocol.'
     license = 'gplv2+'
     inputs = ['logic']
     outputs = ['gpib']
-
-    # keyword "channels" does NOT work
-    optional_channels = (
-        {'id': 'clk', 'name': 'CLK', 'desc': 'Clock line'},
-        {'id': 'd0' , 'name': 'D0', 'desc': 'Data line 0'},
-        {'id': 'd1' , 'name': 'D1', 'desc': 'Data line 1'},
-        {'id': 'd2' , 'name': 'D2', 'desc': 'Data line 2'},
-        {'id': 'd3' , 'name': 'D3', 'desc': 'Data line 3'},
-        {'id': 'd4' , 'name': 'D4', 'desc': 'Data line 4'},
-        {'id': 'd5' , 'name': 'D5', 'desc': 'Data line 5'},
-        {'id': 'd6' , 'name': 'D6', 'desc': 'Data line 6'},
-        {'id': 'd7' , 'name': 'D7', 'desc': 'Data line 7'},
-        {'id': 'EOI', 'name': 'EOI', 'desc': 'End Or Identify, 8'},
-        {'id': 'DAV', 'name': 'DAV', 'desc': 'Clock, falling edge, 9'},
-        {'id': 'NRFD', 'name': 'NRFD', 'desc': 'Not Ready For Data, 10'},
-        {'id': 'NDAC', 'name': 'NDAC', 'desc': 'Not Data Accepted, 11'},
-        {'id': 'IFC', 'name': 'IFC', 'desc': 'InerFace Clear, 12'},
-        {'id': 'SRQ', 'name': 'SRQ', 'desc': 'Service ReQuest, 13'},
-        {'id': 'ATN', 'name': 'ATN', 'desc': 'ATN,Command mode, 14'},
-        {'id': 'REN', 'name': 'REN', 'desc': 'Remote ENable, 15'},
+    # it is mandatory for decoding, that all channels are numbered
+    channels = (
+        {'id': 'd0' , 'name': 'D0', 'desc': 'Data I/O bit 1, ch 0'},
+        {'id': 'd1' , 'name': 'D1', 'desc': 'Data I/O bit 2, ch 1'},
+        {'id': 'd2' , 'name': 'D2', 'desc': 'Data I/O bit 3, ch 2'},
+        {'id': 'd3' , 'name': 'D3', 'desc': 'Data I/O bit 4, ch 3'},
+        {'id': 'd4' , 'name': 'D4', 'desc': 'Data I/O bit 5, ch 4'},
+        {'id': 'd5' , 'name': 'D5', 'desc': 'Data I/O bit 6, ch 5'},
+        {'id': 'd6' , 'name': 'D6', 'desc': 'Data I/O bit 7, ch 6'},
+        {'id': 'd7' , 'name': 'D7', 'desc': 'Data I/O bit 8, ch 7'},
+        {'id': 'eoi', 'name': 'EOI8', 'desc': 'End or identify, ch 8'},
+        {'id': 'dav', 'name': 'DAV9', 'desc': 'Data valid (clock), ch 9'},
+        {'id': 'nrfd', 'name': 'NRFD10', 'desc': 'Not ready for data, ch 10'},
+        {'id': 'ndac', 'name': 'NDAC11', 'desc': 'Not data accepted, ch 11'},
+        {'id': 'ifc', 'name': 'IFC12', 'desc': 'Interface clear, ch 12'},
+        {'id': 'srq', 'name': 'SRQ13', 'desc': 'Service request, ch 13'},
+        {'id': 'atn', 'name': 'ATN14', 'desc': 'Attention, ch 14'},
+        {'id': 'ren', 'name': 'REN15', 'desc': 'Remote enable, ch 15'},
     )
     options = (
-        {'id': 'sample_total', 'desc': 'total number of samples', 'default': 0},
-        {'id': 'debug', 'desc': 'debug print',
-            'default': 'False', 'values': ('False', 'True')},
+        {'id': 'sample_total', 'desc': 'Total number of samples', 'default': 0},
+        {'id': 'state', 'desc': 'state analysis','default': 'False', 'values': ('False', 'True')},
     )
     annotations = (
         ('items', 'Items'),
-         ('gpib', 'DAT/CMD'),
-         ('eoi', 'EOI'),
+        ('gpib', 'DAT/CMD'),
+        ('eoi', 'EOI'),
     )
     annotation_rows = (
-        ('items', 'Bytes', (0,)),
+        ('bytes', 'Bytes', (0,)),
         ('gpib', 'DAT/CMD', (1,)),
         ('eoi', 'EOI', (2,)),
     )
 
     def __init__(self):
-        self.oldclk = None
+        self.olddav = None
         self.items = []
         self.itemcount = 0
         self.saved_item = None
@@ -109,18 +74,15 @@ class Decoder(srd.Decoder):
         self.oldpins = None
         self.ss_item = self.es_item = None
         self.first = True
-        self.last = False
-        self.debug = False
+        self.state = False
 
     def start(self):
-        strdebug = self.options['debug']
-        if strdebug == 'True': self.debug = True
-        if self.debug: print('start GPIB decoder')
-        self.out_python = self.register(srd.OUTPUT_PYTHON)
         self.out_ann = self.register(srd.OUTPUT_ANN)
-
-    def putpb(self, data):
-        self.put(self.ss_item, self.es_item, self.out_python, data)
+        strState = self.options['state']
+        if strState == 'True': 
+            self.state = True
+        if self.state: 
+            print('GPIB State Analysis')
 
     def putb(self, data):
         self.put(self.ss_item, self.es_item, self.out_ann, data)
@@ -136,17 +98,18 @@ class Decoder(srd.Decoder):
             self.ss_word = self.samplenum
 
         # Get the bits for this item.
-        item, used_pins = 0, datapins.count(b'\x01') + datapins.count(b'\x00')
-        for i in range(used_pins):
+        item = 0
+        for i in range(8):
             item |= datapins[i] << i
 
-        item = item & 0xff  # mask 8-bit data byte
-        item = item ^ 0xff  # invert data byte
-        #if self.debug: print('%02X' % item)
+        item = item ^ 0xff # Invert data byte.
         self.items.append(item)
         self.itemcount += 1
-        if datapins[14] == 0: item2 = True 
-        if datapins[8] == 0: item3 = True
+
+        if datapins[14] == 0:
+            item2 = True
+        if datapins[8] == 0:
+            item3 = True
 
         if self.first:
             # Save the start sample and item for later (no output yet).
@@ -155,19 +118,17 @@ class Decoder(srd.Decoder):
             self.saved_item = item
             self.saved_ATN = item2
             self.saved_EOI = item3
-
         else:
             # Output the saved item (from the last CLK edge to the current).
             dbyte = self.saved_item
             dATN = self.saved_ATN
             dEOI = self.saved_EOI
             self.es_item = self.samplenum
-            #self.putpb(['ITEM', self.saved_item])    # to Python output
-            self.putb([0, ['%02X' % self.saved_item]]) # to annotation row = 0
+            self.putb([0, ['%02X' % self.saved_item]]) # to annotation row 0
 
-            # here encode item byte to GPIB convention
+            # Encode item byte to GPIB convention.
             self.strgpib = ' '
-            if dATN:  # ATN, decode commands
+            if dATN: # ATN, decode commands.
                 if dbyte == 0x01: self.strgpib = 'GTL'
                 if dbyte == 0x04: self.strgpib = 'SDC'
                 if dbyte == 0x05: self.strgpib = 'PPC'
@@ -180,69 +141,61 @@ class Decoder(srd.Decoder):
                 if dbyte == 0x19: self.strgpib = 'SPD'
                 if dbyte == 0x3f: self.strgpib = 'UNL'
                 if dbyte == 0x5f: self.strgpib = 'UNT'
-                if dbyte > 0x1f and dbyte < 0x3F:    # address Listener
+                if dbyte > 0x1f and dbyte < 0x3f: # Address Listener.
                     self.strgpib = 'L' + chr(dbyte + 0x10)
-                if dbyte > 0x3f and dbyte < 0x5F:    # address Talker
+                if dbyte > 0x3f and dbyte < 0x5f: # Address Talker
                     self.strgpib = 'T' + chr(dbyte - 0x10)
             else:
-                if dbyte > 0x1f and dbyte < 0x7F: self.strgpib = chr(dbyte)
-                if dbyte == 0x0a: self.strgpib = 'LF'
-                if dbyte == 0x0d: self.strgpib = 'CR'
-                    
-            self.putb([1, [self.strgpib]])  # annotation row = 1
+                if dbyte > 0x1f and dbyte < 0x7f:
+                    self.strgpib = chr(dbyte)
+                if dbyte == 0x0a:
+                    self.strgpib = 'LF'
+                if dbyte == 0x0d:
+                    self.strgpib = 'CR'
+
+            self.putb([1, [self.strgpib]]) # to annotation row 1
             self.strEOI = ' '
-            if dEOI: 
+            if dEOI:
                 self.strEOI = 'EOI'
-            self.putb([2, [self.strEOI]])  # annotation row = 2
-            # to Python output
-            #self.putpb(['ITEM', self.saved_item])
-            #self.putpb(['GPIB', self.strgpib])
-            if self.debug: print("{0:6d}; {1:02X};{2:>4d}; {3:4};{4:>4}".format(self.ss_item,dbyte,dbyte,self.strgpib,self.strEOI))
+            self.putb([2, [self.strEOI]]) # to annotation row 2
+            
+            if self.state:
+                print("{0:6d}; {1:02X};{2:>4d}; {3:4};{4:>4}".format(self.ss_item,dbyte,dbyte,self.strgpib,self.strEOI))
 
             self.ss_item = self.samplenum
             self.saved_item = item
             self.saved_ATN = item2
             self.saved_EOI = item3
 
-        ws = 16 # must be 16 pins for GPIB
-
-        # Get as many items as the configured wordsize says.
-        if self.itemcount < ws:
+        if self.itemcount < 16:
             return
 
         self.itemcount, self.items = 0, []
 
-    def find_clk_edge(self, clk, datapins):
-        # Ignore sample if the clock pin hasn't changed.
-        if clk == self.oldclk:
+    def find_falling_dav_edge(self, dav, datapins):
+        # Ignore sample if the DAV pin hasn't changed.
+        if dav == self.olddav:
             return
-        self.oldclk = clk
-        # sample on falling clock edge
-        if clk == 1: 
+        self.olddav = dav
+        # Sample on falling DAV edge.
+        if dav == 1:
             return
 
-        # Found the correct clock edge, now get the bits.
+        # Found the correct DAV edge, now get the bits.
         self.handle_bits(datapins)
 
     def decode(self, ss, es, data):
-        #if self.debug: print(self.samplenum, ' ', ss, ' ', es) # see the chunks
+        #print(self.samplenum, ' ', ss, ' ', es) # see the chunks
         lsn = self.options['sample_total']
 
         for (self.samplenum, pins) in data:
             if lsn > 0:
-                if (lsn - self.samplenum) == 1:  # show last data word
-                    self.handle_bits(pins[1:])
+                if (lsn - self.samplenum) == 1: # Show the last data word.
+                    self.handle_bits(pins)
 
             # Ignore identical samples early on (for performance reasons).
             if self.oldpins == pins:
                 continue
             self.oldpins = pins
 
-            if sum(1 for p in pins if p in (0, 1)) == 0:
-                raise ChannelError('At least one channel has to be supplied.')
-
-            if pins[0] not in (0, 1):
-                self.handle_bits(pins[1:])  
-            else:
-                self.find_clk_edge(pins[0], pins[1:])
-
+            self.find_falling_dav_edge(pins[9], pins)
